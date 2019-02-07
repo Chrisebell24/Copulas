a4accb846b03cf7dab6b5f4a33d783887ff58b35
Chrisebell24 <31806186+Chrisebell24@users.noreply.github.com>
2019-02-06 19:10:45 -0600

    Update example.py
    Changed example to reflect newest pip install

--- examples/example.py
+++ examples/example.py
@@ -2,14 +2,14 @@ import logging
 
 import pandas as pd
 
-from copulas.multivariate.GaussianCopula import GaussianCopula
+from copulas.multivariate import GaussianMultivariate
 
 LOGGER = logging.getLogger(__name__)
 
 
 if __name__ == '__main__':
     data = pd.read_csv('data/iris.data.csv')
-    gc = GaussianCopula()
+    gc = GaussianMultivariate()
     gc.fit(data)
     LOGGER.debug(gc.sample(num_rows=1))
-    LOGGER.debug(gc.cov_matrix)
+    LOGGER.debug(gc.covariance)
